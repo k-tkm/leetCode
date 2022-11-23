040735e15468b2521a87fd4c6597443a9ab57025
k-tkm <kjohlkjou89@gmail.com>
2022-11-23 23:02:34 +0900

    add solution

--- typescript/13.roman-to-integer.ts
+++ typescript/13.roman-to-integer.ts
@@ -5,5 +5,32 @@
  */
 
 // @lc code=start
-function romanToInt(s: string): number {}
+function romanToInt(s: string): number {
+  const symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  const replaceStr = (str: string) => {
+    str.replace("IV", "IIII");
+    str.replace("IX", "VIIII");
+    str.replace("XL", "XXXX");
+    str.replace("XC", "LXXXX");
+    str.replace("CD", "CCCC");
+    str.replace("CM", "DCCCC");
+    return str;
+  };
+
+  const replacedStr = replaceStr(s);
+  let result: number = 0;
+  for (const str of replacedStr) {
+    result = result + symbols[str];
+  }
+  return result;
+}
 // @lc code=end
