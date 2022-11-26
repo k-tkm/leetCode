1963203ca58880c92efab94ca053b7d30ee44ada
k-tkm <kjohlkjou89@gmail.com>
2022-11-26 13:56:23 +0900

    vertical scanning

--- typescript/14.longest-common-prefix.ts
+++ typescript/14.longest-common-prefix.ts
@@ -7,13 +7,15 @@
 // @lc code=start
 function longestCommonPrefix(strs: string[]): string {
   if (strs.length === 0) "";
-  let prefix = strs[0];
-  for (let i = 1; i < strs.length; i++) {
-    while (strs[i].indexOf(prefix) !== 0) {
-      prefix = prefix.substring(0, prefix.length - 1);
-      if (prefix.length === 0) "";
+  for (let i = 0; i < strs[0].length; i++) {
+    let c: string = strs[0].charAt(i);
+    for (let j = 1; j < strs.length; j++) {
+      if (i === strs[j].length || strs[j].charAt(i) !== c) {
+        return strs[0].substring(0, i);
+      }
     }
   }
-  return prefix;
+  return strs[0];
 }
+// longestCommonPrefix(["flower", "flow", "flight"]);
 // @lc code=end
