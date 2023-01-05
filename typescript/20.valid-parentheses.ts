0df001a55326720f2df8cb90d40ba1d6e917a730
k-tkm <kjohlkjou89@gmail.com>
2023-01-05 16:24:07 +0900

    Correct solution

--- typescript/20.valid-parentheses.ts
+++ typescript/20.valid-parentheses.ts
@@ -5,27 +5,21 @@
  */
 
 // @lc code=start
-let preStr;
-const validate = (str: string) => {
-  if (preStr === "(") {
-    return str === ")";
-  }
-  if (preStr === "{") {
-    return str === "}";
-  }
-  if (preStr === "[") {
-    return str === "]";
-  }
-  return false;
-};
+
 function isValid(s: string): boolean {
+  const left: string[] = [];
+  const legend = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
   for (let i = 0; i < s.length; i++) {
-    if (i % 2 === 1) {
-      return validate(s[i]);
-    } else {
-      preStr = s[i];
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      left.push(s[i]);
+    } else if (legend[left.pop() || ""] !== s[i]) {
+      return false;
     }
   }
-  return false;
+  return left.length ? false : true;
 }
 // @lc code=end
