8006cadc296252efdc091709bfb19538b863bc40
k-tkm <kjohlkjou89@gmail.com>
2023-01-07 22:09:50 +0900

    solution

--- typescript/21.merge-two-sorted-lists.ts
+++ typescript/21.merge-two-sorted-lists.ts
@@ -17,8 +17,32 @@
  * }
  */
 
-function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+// class ListNode {
+//   val: number;
+//   next: ListNode | null;
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.next = next === undefined ? null : next;
+//   }
+// }
 
-};
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  console.log("call --------", list1, list2);
+  if (!list1 || !list2) {
+    return list1 || list2;
+  }
+
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    console.log("call top", list1, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    console.log("call bottom", list2);
+    return list2;
+  }
+}
 // @lc code=end
-
